2f54f4f21574982ff021fdf138dcd9e8faba3e11
Radmil <radmil.aliacberov@gmail.com>
2024-11-14 20:04:47 +0400

    modified:   package-lock.json 	modified:   package.json 	modified:   public/img/card/card1.jpeg 	new file:   public/img/card/card2.jpeg 	new file:   public/img/card/card3.jpeg 	new file:   public/img/card/card4.jpeg 	modified:   public/img/logo.png 	modified:   src/App.js 	modified:   src/index.js

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ function App() {
   return (
     <div className="wrapper clear">
        <header className="d-flex justify-between align-center p-40">
-        <div className = "d-flex align-center"> 
+        <div className = "d-flex align-center pl-20"> 
         <img width = {100} height = {100} src = "/img/logo.png" />
-        <div>
-          <h3 className="text-uppercase">
-            React Sneakers</h3>
+        <div className = "pl-30">
+          <h3 className="">
+            New Balance</h3>
           <p>
-            Магазин лучших кросовок</p>
+            For Sience 1899</p>
          </div> 
         </div>
         <ul className = "d-flex">
-         <img width = {20} height = {20 } src = "/img/basket.png"/>
-         <span className="pr-20">1205 rub</span>
+         <img  width = {20} height = {20 } src = "/img/basket.png"/>
+         <span className="pr-10 pl-10 ">1205 rub</span>
         <li>
         <li className="mr-30">
           <img width = {20} height = {20} src = "/img/user.png"/>
@@ -29,7 +29,7 @@ function App() {
           <div className = "d-flex">
 
           <div className="card">
-            <img width={133}  height={160} src="/img/card/card1.jpeg" alt=""></img>
+            <img width={150}  height={160} src="/img/card/card1.jpeg" alt=""></img>
             <h5>Мужские кроссовки Nike Blazer Mid Suede </h5>
             <div className="d-flex justify-between align-center pt-20">
               <div className="d-flex flex-column r">
@@ -43,7 +43,7 @@ function App() {
           </div>
 
           <div className="card">
-            <img width={133}  height={160} src="/img/card/card1.jpeg" alt=""></img>
+            <img width={150}  height={160} src="/img/card/card2.jpeg" alt=""></img>
             <h5>Мужские кроссовки Nike Blazer Mid Suede </h5>
             <div className="d-flex justify-between align-center pt-20">
               <div className="d-flex flex-column r">
@@ -56,7 +56,7 @@ function App() {
             </div>
           </div>
           <div className="card">
-            <img width={133}  height={160} src="/img/card/card1.jpeg" alt=""></img>
+            <img width={150}  height={160} src="/img/card/card3.jpeg" alt=""></img>
             <h5>Мужские кроссовки Nike Blazer Mid Suede </h5>
             <div className="d-flex justify-between align-center pt-20">
               <div className="d-flex flex-column r">
@@ -69,7 +69,7 @@ function App() {
             </div>
           </div>
           <div className="card">
-            <img width={133}  height={160} src="/img/card/card1.jpeg" alt=""></img>
+            <img width={150}  height={160} src="/img/card/card4.jpeg" alt=""></img>
             <h5>Мужские кроссовки Nike Blazer Mid Suede </h5>
             <div className="d-flex justify-between align-center pt-20">
               <div className="d-flex flex-column r">
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.scss';
+import '/Users/radmilaliacberov/Desktop/Git/react-sneakers-1/src/index.scss'
 import 'macro-css'
 import App from './App';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
